23af16c26fe2009cbc867d4b7569e54899eda56b
Mthdk <matheus.karo@aluno.faculdadeimpacta.com.br>
2024-03-27 16:41:02 -0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,9 +1,10 @@
 SELECT 
-  CASE 
-    WHEN REGEXP_REPLACE(imagem, '.*_([0-9]{9})\.tif$', '\1') IS NULL THEN 'SEM KICID' 
-    WHEN REGEXP_LIKE(imagem, '_[0-9]{9}\.tif$') THEN 'KICID INCORRETO'
-    ELSE REGEXP_REPLACE(imagem, '.*_([0-9]{9})\.tif$', '\1') 
-  END AS KICID_REGEX
+  LENGTH(REGEXP_REPLACE(proc.imagem, '[^_]', '')) AS teste,
+  REGEXP_INSTR(REGEXP_REPLACE(proc.imagem, '\.tif$', ''), '_[^_]*$', 1, 1, 0, 'c') AS POS,
+  CASE
+    WHEN LENGTH(REGEXP_REPLACE(proc.imagem, '[^_]', '')) = 2 THEN REGEXP_REPLACE(REGEXP_SUBSTR(proc.imagem, '_[^_]*$', 1, 1), '\.tif$', '')
+    ELSE '0'
+  END AS KICID_REPLACE
 FROM (
   SELECT '27032024_16_123456789.tif' AS imagem FROM dual UNION ALL
   SELECT '27032024_16_1234_56789.tif' FROM dual UNION ALL
