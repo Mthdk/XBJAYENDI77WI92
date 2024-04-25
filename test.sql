f22678d44ab08fbff65b2674e53f06cd531bdac7
Mthdk <matheus.karo@aluno.faculdadeimpacta.com.br>
2024-04-25 13:14:17 -0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,14 +1,13 @@
-SELECT 
-  LENGTH(REGEXP_REPLACE(proc.imagem, '[^_]', '')) AS teste,
-  REGEXP_INSTR(REGEXP_REPLACE(proc.imagem, '\.tif$', ''), '_[^_]*$', 1, 1, 0, 'c') AS POS,
-  CASE
-    WHEN LENGTH(REGEXP_REPLACE(proc.imagem, '[^_]', '')) = 2 THEN REGEXP_REPLACE(REGEXP_SUBSTR(proc.imagem, '_([^_]*)(?:\.tif)?$', 1, 2, NULL, 1), '\.tif$', '')
-    ELSE '0'
-  END AS KICID_REPLACE
-FROM (
-  SELECT '27032024_16_123456789.tif' AS imagem FROM dual UNION ALL
-  SELECT '27032024_16_1234_56789.tif' FROM dual UNION ALL
-  SELECT '27032024_16.tif' FROM dual UNION ALL
-  SELECT '' FROM dual UNION ALL
-  SELECT '27032024_16_%%123456789.tif' FROM dual
-) proc;
+WITH Documentos AS (
+  SELECT
+    documento_id,
+    COUNT(*) OVER (PARTITION BY documento_id) AS total_paginas
+  FROM paginas_documento
+  WHERE documento_id = 5 -- Aqui você especifica o ID do documento desejado
+)
+SELECT
+  documento_id,
+  imagem,
+  total_paginas
+FROM paginas_documento
+JOIN Documentos ON paginas_documento.documento_id = Documentos.documento_id;
